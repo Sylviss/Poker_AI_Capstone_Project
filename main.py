a69b4f8801fdd4497af64561e068d594712fe37d
Sylviss <76834738+Sylviss@users.noreply.github.com>
2023-10-31 10:03:51 +0700

    Fix some

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from poker_ai.poker.play import game,PREFLOP_BIG_BLIND
 ###############################################
 #Constant:
 
-PLAYER=2
+PLAYER=4
 INIT_MONEY=10*PREFLOP_BIG_BLIND
 
 
 ################################################
-
 
 game(PLAYER,INIT_MONEY)
