6d0655fd17188d459578728a696a9a6e21e35545
Frostedpilot <Truong.VT225535@sis.hust.edu.vn>
2023-12-15 22:35:57 +0700

    finished the basic opponent modelling, I guess

--- poker_ai/ai/ml/opponent_modelling.py
+++ poker_ai/ai/ml/opponent_modelling.py
@@ -6,20 +6,20 @@ class Rate_recorder():
 
 class Data_table():
     def __init__(self):
-        self.counting_table = {i: {j: {k: 0 for k in ['fold', 'check', 'call', 'raise', 'all in']}\
-                        for j in ['preflop', 'mid_game', 'showdown']}\
+        self.counting_table = {i: {j: {k: 1 for k in ['fold', 'check', 'call', 'raise', 'all in']}\
+                        for j in ['preflop', 'flop', 'turn', 'river']}\
                         for i in ['strong', 'medium', 'weak']}
-        self.data_table = {i: {j: {k: 0 for k in ['fold', 'check', 'call', 'raise', 'all in']}\
-                        for j in ['preflop', 'mid_game', 'showdown']}\
+        self.data_table = {i: {j: {k: 1 for k in ['fold', 'check', 'call', 'raise', 'all in']}\
+                        for j in ['preflop', 'flop', 'turn', 'showdown']}\
                         for i in ['strong', 'medium', 'weak']}
-        self.count = 0
+        self.count = 45
     def refresh_table(self):
         self.__init__()
 
 def table_building(history, tables, hs):
     ACTION_TABLE = [None, 'check', 'call', 'call', 'raise', 'raise', 'raise', 'fold', 'all in']
     data = history[-1]
-    counting_table = tables[data[0]]
+    counting_table = tables[data[0]].counting_table
     match data[1]:
         case 0:
             counting_table[hs]['preflop'][ACTION_TABLE[data[2]]] += 1
@@ -51,18 +51,24 @@ def opponent_modelling(history, tables, recorder, turn):
 
     TURN_TABLE = ['preflop',None,None,'flop','turn','river']
 
-    if recorder >= 60:
+    if recorder.win >= 60:
         hs = 'strong'
-    elif 40 <= recorder < 60:
+    elif 40 <= recorder.win < 60:
         hs = 'medium'
     else:
         hs = 'weak'
     tables = table_building(history, tables, hs)
-    shs = [0, 0, 0, 0, 0, 0]
-    sturn = [0, 0, 0, 0, 0, 0]
-    shf, shch, shc, shr, sha = 0, 0, 0, 0, 0
+    tmp = {i:{'phi':[None,0,0,0,0,0],\
+              'phs':[0,0,0,0,0,0],\
+              'pturn':[0,0,0,0,0,0],\
+             } for i in tables}
+    res = {i:[] for i in tables}
     for player in tables:
-        table = tables[player]
+        table = tables[player].counting_table
+        count = tables[player].count
+        shs = [0, 0, 0, 0, 0, 0]
+        sturn = [0, 0, 0, 0, 0, 0]
+        shf, shch, shc, shr, sha = 0, 0, 0, 0, 0
         for _hs in table:
             if _hs == hs:
                 for _turn in table[_hs]:
@@ -160,4 +166,27 @@ def opponent_modelling(history, tables, recorder, turn):
                                     shr += 1
                                 case 'all in':
                                     sha += 1
-    
+        tmp[player]['phi'][1] = shf/count
+        tmp[player]['phi'][2] = shch/count
+        tmp[player]['phi'][3] = shc/count
+        tmp[player]['phi'][4] = shr/count
+        tmp[player]['phi'][5] = sha/count
+        tmp[player]['phs'][0] = shs[0]/count
+        tmp[player]['phs'][1] = shs[1]/count
+        tmp[player]['phs'][2] = shs[2]/count
+        tmp[player]['phs'][3] = shs[3]/count
+        tmp[player]['phs'][4] = shs[4]/count
+        tmp[player]['phs'][5] = shs[5]/count
+        tmp[player]['pturn'][0] = sturn[0]/count
+        tmp[player]['pturn'][1] = sturn[1]/count
+        tmp[player]['pturn'][2] = sturn[2]/count
+        tmp[player]['pturn'][3] = sturn[3]/count
+        tmp[player]['pturn'][4] = sturn[4]/count
+        tmp[player]['pturn'][5] = sturn[5]/count
+
+    for player in tables:
+        print(tmp[player])
+        for i in range(1,6):
+            bruh = ((tmp[player]['phi'][i])*(tmp[player]['phs'][i])*(tmp[player]['pturn'][i]))/((tmp[player]['phs'][0])*(tmp[player]['pturn'][0]))
+            res[player].append(bruh)
+    return res
