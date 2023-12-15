86698bc88375a9e029904b140b4c9dadebf076d3
your_username <117010350+lht11062004@users.noreply.github.com>
2023-12-15 21:02:09 +0700

    Update Bot1 vs Bot-1.py

--- win_rate/Bot1 vs Bot-1.py
+++ win_rate/Bot1 vs Bot-1.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt 
 
-bot_1 = [0.467 , 0.54, 0.53, 0.56, 0.47, 0.48, 0.47, 0.41, 0.553, 0.486]
-bot_minus1 = [0.533, 0.46, 0.47, 0.44, 0.53, 0.52, 0.53, 0.59, 0.447, 0.514]
+bot_1 = [0.49, 0.485, 0.48, 0.5, 0.48, 0.495, 0.59, 0.56, 0.445]
+bot_minus1 = [0.51, 0.515, 0.52, 0.5, 0.52, 0.505, 0.41, 0.44, 0.555]
 
-update_weight = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1']
+update_weight = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9']
 
 plt.plot(update_weight, bot_minus1, label='Win rate of bot -1', marker = 'o', markerfacecolor = 'blue')
 plt.plot(update_weight, bot_1, label='Win rate of bot 1', marker = 'o', markerfacecolor = 'red')
