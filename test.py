a71937382a5edcbbce16cbb35b913fefc9b0fc51
Sylviss <76834738+Sylviss@users.noreply.github.com>
2023-12-22 09:27:33 +0700

    update and add training function

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
 import json
 
 def check_validity(data):
+    a=99999
     for hand in data:
         for turn in data[hand]:
             for check in data[hand][turn]:
                 for action in data[hand][turn][check]:
-                    if data[hand][turn][check][action] == 0:
-                        return False
-    return True
+                    a=min(data[hand][turn][check][action],a)
+    return a
 
-if __name__ == '__main__':
+def main():
     try:
-        f = open("poker_ai/ai/ml/play_data.json")
+        with open("poker_ai/ai/ml/play_data.json") as f:
+            data = json.load(f)
+            for hand in data:
+                for turn in data[hand]:
+                    for check in data[hand][turn]:
+                        for action in data[hand][turn][check]:
+                            data[hand][turn][check][action] = round(data[hand][turn][check][action])
+            return check_validity(data)
     except:
         print('No file exist!')
-    else:
-        data = json.load(f)
-        for hand in data:
-            for turn in data[hand]:
-                for check in data[hand][turn]:
-                    for action in data[hand][turn][check]:
-                        data[hand][turn][check][action] = round(data[hand][turn][check][action])
-        print(data)
-        print(check_validity(data))
-        f.close()
+if __name__ == '__main__':
+    main()
--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
-from poker_ai.poker.play import fast_testing,game_but_cheaty,dataset_logging
+from poker_ai.poker.play import fast_testing,game_but_cheaty
 import sys,os
-
+from test import main
 # Disable print
 def blockPrint():
     sys.stdout = open(os.devnull, 'w', encoding="utf-8")
@@ -36,7 +36,19 @@ def test_single_game():
 def test_module():
     fast_testing(2,100,[1,-1])
     
-def dataset_logger():
-    dataset_logging(2,1000,[1,-1])
+    
+def training(n):
+    if n==2:
+        hehe=[1,-1]
+    elif n==4:
+        hehe=[1,0,-1,-1]
+    else:
+        hehe=[1,0,-1,1,-1,0]
+    while True:
+        fast_testing(n,1000,hehe)
+        print(main()) 
+
+
+
 if __name__=="__main__":
-    test_module()
+    training(2)
